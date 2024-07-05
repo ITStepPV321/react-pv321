0382261c4b49e852c5948f5afe5322c7869aefa3
TSShrol <shrolts@gmail.com>
2024-07-05 18:50:28 +0300

    added useForm

--- src/components/books/NewBook.js
+++ src/components/books/NewBook.js
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate, useOutletContext } from "react-router-dom";
 export default function NewBook() {
     const navigate = useNavigate();
@@ -40,47 +41,59 @@ export default function NewBook() {
     // }
 
     //=====================2. using useRef (React.createRef()) in functional (class) Components  = > ref binding to input
-    const titleRef = useRef(""); // {current: ""}
+    const titleRef = useRef(null); // {current: ""}
     console.log(titleRef.current);
-    const pagesRef = useRef(0); // {current: 0}
+    const pagesRef = useRef(null); // {current: 0}
     console.log(pagesRef.current);
 
-    //===================== for 1 and 2
-    const submit = (event) => {
-        event.preventDefault();
-        // console.log(event.target);
-        // newBook.title = document.getElementById("title").value;
-        // newBook.pages = document.getElementById("pages").value;
-        // setNewBook(newBook);
-        //for variant 2 = > usrRef
-        //read ref data
-        // console.dir(titleRef.current);
-        // console.log(titleRef.current.value);
-        // console.log(pagesRef.current.value);
-        newBook.title=titleRef.current.value;
-        newBook.pages=pagesRef.current.value;
+    //===================== for variant 1 and 2
+    // const submit = (event) => {
+    //     event.preventDefault();
+    //     // console.log(event.target);
+    //     // newBook.title = document.getElementById("title").value;
+    //     // newBook.pages = document.getElementById("pages").value;
+    //     // setNewBook(newBook);
+    //     //for variant 2 = > usrRef
+    //     //read ref data
+    //     // console.dir(titleRef.current);
+    //     // console.log(titleRef.current.value);
+    //     // console.log(pagesRef.current.value);
+    //     newBook.title=titleRef.current.value;
+    //     newBook.pages=pagesRef.current.value;
 
-        setNewBook({...newBook});
-        console.log(newBook);
-        setBooks([...books, newBook]);
-        // navigate to
-        navigate("/books");
+    //     setNewBook({...newBook});
+    //     console.log(newBook);
+    //     setBooks([...books, newBook]);
+    //     // navigate to
+    //     navigate("/books");
+    // }
+
+    // =============3.  using hook useForm() in  react-hook-form===========
+    const {register, handleSubmit}=useForm();
+    const onSubmit=(addBook)=>{
+        console.log(addBook);
     }
+
     return (
         <>
             <div className="form-newBook">
-                <form action="" onSubmit={submit}>
+                {/* <form action="" onSubmit={submit}> */}
+                <form action="" onSubmit={handleSubmit(onSubmit)}>
                     <div className="container-input">
                         <label htmlFor="title" >Title: </label>
                         {/* <input type="text" id="title" name="title" onChange={handleTitleChange} value={newBook.title} /> */}
                         {/* <input type="text" id="title" name="title" onChange={handleInputChange} value={newBook.title} /> */}
-                        <input ref={titleRef} type="text" id="title" name="title"/>
+                        {/* <input ref={titleRef} type="text" id="title" name="title"/> */}
+                        <input {...register("title")} type="text" id="title"/>
+                        
                     </div>
                     <div className="container-input">
                         <label> <span>Pages:</span>
                             {/* <input type="number" min="0" id="pages" name="pages" onChange={handlePagesChange} value={newBook.pages} /> */}
                             {/* <input type="number" min="0" id="pages" name="pages" onChange={handleInputChange} value={newBook.pages} /> */}
-                            <input ref={pagesRef} type="number" min="0" id="pages" name="pages" />
+                            {/* <input ref={pagesRef} type="number" min="0" id="pages" name="pages" /> */}
+                        <input {...register("pages")} type="number" id="pages"/>
+
                         </label>
                     </div>
                     <div className="container-button">
